e655c6caa779237d1c64ca02a4e6513fa49d229f
Kade Robertson <kade.robertson16@gmail.com>
2018-01-09 13:41:04 -0500

    didnt use key properly, bugfixing

--- commands/weather.py
+++ commands/weather.py
@@ -21,15 +21,15 @@ class Weather(CommandBase):
         ]
     def get_help_msg(self, cmd):
         return "Call /{} <city>,<country> where country is the 2-letter country code.".format(cmd)
-    def load_config(self, confdict);
+    def load_config(self, confdict):
         self.settings = {
             "units": "metric",
-            "lang": en,
+            "lang": "en",
             "APPID": confdict["api_key"]
         }
     # https://gist.github.com/RobertSudwarts/acf8df23a16afdb5837f
     # Ultra crap but it'll do for now.
-    def card(d):
+    def card(self, d):
         dirs = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
                 "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
         ix = int((d + 11.25)/22.5 - 0.02)
@@ -42,20 +42,23 @@ class Weather(CommandBase):
                                  text = "This doesn't seem like correct usage of /weather.",
                                  disable_notification = True)
                 return
-            data = owm.get_current(args[1])
+            data = owm.get_current(args[1], **self.settings)
             temp, tmin, tmax = data('main.temp', 'main.temp_min', 'main.temp_max')
-            form = ("Weather for {}, {}:\n".format(data['id']['name'], data['sys']['country'])
-                    " - {}\n".format(data['weather']['description'].capitalize())
-                    " - Cur {.1f} / High {.1f} / Low {.1f}\n".format(temp, tmin, tmax)
-                    " - Wind: {} km/h {} ".format(data['wind']['speed'], self.card(data['wind']['deg']))
-                    "(Gust: {} km/h)".format(data['wind']['gust']))
+            form = "Weather for {}, {}:\n".format(data['name'], data['sys']['country'])
+            form += " - {}\n".format(data['weather'][0]['description'].capitalize())
+            form += " - Cur {:.1f}°C / High {:.1f}°C / Low {:.1f}°C\n".format(temp, tmin, tmax)
+            form += " - Wind: {} km/h {}".format(data['wind']['speed'], self.card(data['wind']['deg']))
+            if 'gust' in data['wind']:
+                form += " (Gust: {} km/h)".format(data['wind']['gust'])
             bot.send_message(chat_id = update.message.chat_id,
-                             text = "This doesn't seem like correct usage of /weather.",
+                             text = form,
                              disable_notification = True)
             self.logger.info("Command /weather executed successfully.")
-        except urllib.error.HTTPError:
-            bot.send_message(chat_id = update.message.chat_id,
-                             text = "The location you chose seems to be invalid.",
-                             disable_notification = True)
+        #except urllib.error.HTTPError:
+        #    bot.send_message(chat_id = update.message.chat_id,
+        #                     text = "The location you chose seems to be invalid.",
+        #                     disable_notification = True)
         except Exception as e:
+            raise e
+            print(e)
             self.logger.error(e)
