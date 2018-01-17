2b6d6b4289bf53b805d46a479d831d89061a6306
Kade Robertson <kade.robertson16@gmail.com>
2018-01-17 08:46:34 -0500

    initial breed implementation

--- commands/dog.py
+++ commands/dog.py
@@ -15,10 +15,17 @@ class Dog(CommandBase):
             CommandInfo("dog", self.execute, "Displays a random dog image.")
         ]
     def get_help_msg(self, cmd):
-        return "Call /dog with no arguments."
+        return ("Call /dog with no arguments for a general random image.\n"
+                "Call /dog <breed> to get a random image for a specific breed.")
     def execute(self, bot, update):
         try:
-            data = requests.get("https://dog.ceo/api/breeds/image/random").json()
+            args = shlex.split(update.message.text)
+            if len(args) == 1:
+                data = requests.get("https://dog.ceo/api/breeds/image/random").json()
+            elif len(args) == 2:
+                name = args[1].lower().split(' ')[::-1].join('/')
+                urlfmt = "https://dog.ceo/api/breed/{0}/images/random"
+                data = requests.gete(urlfmt.format(name))).json()
             bot.send_photo(chat_id = update.message.chat_id, 
                            photo = data["message"],
                            disable_notification = True)
