cd913693edec7c605ba4cf17e25a236ace88a669
Kade Robertson <kade.robertson16@gmail.com>
2018-01-17 09:10:31 -0500

    support Ibizan weirdness

--- commands/dog.py
+++ commands/dog.py
@@ -18,17 +18,25 @@ class Dog(CommandBase):
     def get_help_msg(self, cmd):
         return ("Call /dog with no arguments for a general random image.\n"
                 "Call /dog <breed> to get a random image for a specific breed.")
+    def dogify(self, lst):
+        out = ""
+        for item in lst[::-1]:
+            if item == 'ibizan':
+                out += 'Ibizan/'
+            else:
+                out += item + '/'
+        return out[:-1]
     def execute(self, bot, update):
         try:
             args = shlex.split(update.message.text)
             if len(args) == 1:
                 data = requests.get("https://dog.ceo/api/breeds/image/random").json()
             elif len(args) == 2:
-                name = '/'.join(args[1].lower().split(' ')[::-1])
+                name = self.dogify(args[1].lower().split(' ')[::-1])
                 urlfmt = "https://dog.ceo/api/breed/{0}/images/random"
                 data = requests.get(urlfmt.format(name)).json()
             else:
-                name = '/'.join([x.lower() for x in args[1:]][::-1])
+                name = self.dogify([x.lower() for x in args[1:]])
                 urlfmt = "https://dog.ceo/api/breed/{0}/images/random"
                 data = requests.get(urlfmt.format(name)).json()
             bot.send_photo(chat_id = update.message.chat_id, 
