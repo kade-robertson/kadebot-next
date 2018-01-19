ce0ca9ef7c8ce7353d517e88040c7e9e2bced9e3
Kade Robertson <kade@kaderobertson.pw>
2018-01-19 15:55:26 -0500

    actually implement properly you numpty

--- commands/grt.py
+++ commands/grt.py
@@ -6,14 +6,18 @@
 
 import shlex
 import requests
+from .basic import CommandBase, CommandInfo
 
-class GRT(BaseCommand):
+class GRT(CommandBase):
     name = "GRT"
     safename = "grt"
     def __init__(self, logger):
         super().__init__(logger)
+        self.to_register = [
+            CommandInfo("grt", self.execute, "Get bus times for a stop and bus number"),
+        ]
     def get_help_msg(self, cmd):
-        return "Call /grt <stop> <bus> to check bus times.
+        return "Call /grt <stop> <bus> to check bus times."
     def execute(self, bot, update):
         try:
             args = shlex.split(update.message.text)
@@ -24,15 +28,15 @@ class GRT(BaseCommand):
                 return
             params = { 'stopId': args[1], 'routeId': args[2] }
             data = requests.get(r"http://realtimemap.grt.ca/Stop/GetStopInfo", params=params).json()
-            times = data['stopTimes']
+            times = [(x['TripId'], x['Minutes']) for x in data['stopTimes']]
             if len(times) == 0:
                 bot.send_message(chat_id = update.message.chat_id,
                                  text = "There are no times available for this query.",
                                  disable_notification = True)
                 return
-            times = sorted(set(times), key=lamdba x: x['Minutes'])
+            times = sorted(set(times), key=lambda x: x[1])
             output = "Times for the {} at stop {}: ".format(args[2], args[1])
-            output += ", ".join(x['Minutes'] + ' minutes' for x in times)
+            output += ", ".join(str(x[1]) + ' minutes' for x in times)
             bot.send_message(chat_id = update.message.chat_id,
                              text = output,
                              disable_notification = True)
