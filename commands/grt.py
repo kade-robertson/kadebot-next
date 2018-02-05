d37ab67d28fe326f0de8762d4ca49293f2d388fb
Kade Robertson <kade@kaderobertson.pw>
2018-02-05 08:30:42 -0500

    update grt

--- commands/grt.py
+++ commands/grt.py
@@ -18,28 +18,25 @@ class GRT(CommandBase):
         ]
     def get_help_msg(self, cmd):
         return "Call /grt <stop> <bus> to check bus times."
-    def execute(self, bot, update):
-        try:
-            args = shlex.split(update.message.text)
-            if len(args) != 3:
-                bot.send_message(chat_id = update.message.chat_id,
-                                 text = "This doesn't seem like correct usage of /grt.",
-                                 disable_notification = True)
-                return
-            params = { 'stopId': args[1], 'routeId': args[2] }
-            data = requests.get(r"http://realtimemap.grt.ca/Stop/GetStopInfo", params=params).json()
-            times = [(x['TripId'], x['Minutes']) for x in data['stopTimes']]
-            if len(times) == 0:
-                bot.send_message(chat_id = update.message.chat_id,
-                                 text = "There are no times available for this query.",
-                                 disable_notification = True)
-                return
-            times = sorted(set(times), key=lambda x: x[1])
-            output = "Times for the {} at stop {}: ".format(args[2], args[1])
-            output += ", ".join(str(x[1]) + ' minute(s)' for x in times)
+    @log_error
+    def execute(self, bot, update, **kwargs):
+        args = kwargs.get('args')
+        if len(args) != 2:
             bot.send_message(chat_id = update.message.chat_id,
-                             text = output,
+                             text = "This doesn't seem like correct usage of /grt.",
                              disable_notification = True)
-            self.logger.info("Command /grt executes successfully.")
-        except Exception as e:
-            self.logger.error(e)
+            return
+        params = { 'stopId': args[0], 'routeId': args[1] }
+        data = requests.get(r"http://realtimemap.grt.ca/Stop/GetStopInfo", params=params).json()
+        times = [(x['TripId'], x['Minutes']) for x in data['stopTimes']]
+        if len(times) == 0:
+            bot.send_message(chat_id = update.message.chat_id,
+                                text = "There are no times available for this query.",
+                                disable_notification = True)
+            return
+        times = sorted(set(times), key=lambda x: x[1])
+        output = "Times for the {} at stop {}: ".format(args[1], args[0])
+        output += ", ".join(str(x[1]) + ' minute(s)' for x in times)
+        bot.send_message(chat_id = update.message.chat_id,
+                            text = output,
+                            disable_notification = True)
