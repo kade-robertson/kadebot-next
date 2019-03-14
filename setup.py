5fc797005967362c072e3ead7827436c836ce482
Kade <kade-robertson@users.noreply.github.com>
2019-03-14 19:20:34 -0400

    Revert "Use proper configuration/data/cache directories"

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 import src
 
@@ -8,10 +8,6 @@ try:
     long_desc = pypandoc.convert('README.md', 'rst', extra_args=('--eol', 'lf'))
 except (IOError, ImportError):
     long_desc = open('README.md').read()
-
-requirements = []
-with open('requirements.txt') as reqs:
-    requirements = [req.strip() for req in reqs.readlines()]
 
 setup(
     name="kadebot",
@@ -24,5 +20,5 @@ setup(
     author="Kade Robertson",
     author_email="kade@kaderobertson.pw",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=['python-telegram-bot>=11.1.0'],
     python_requires='>=3.6, <4')
--- src/__init__.py
+++ src/__init__.py
@@ -2,29 +2,23 @@
 
 import argparse
 import logging
-import os
 
-import configbetter
-from telegram.ext import MessageHandler, Updater
+from telegram.ext import Updater, MessageHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-conf = configbetter.Config('kadebot-next')
-
 
 def setup_bot(args):
     pass
 
 
 def main():
-    conf.makedirs()
     parser = argparse.ArgumentParser(description='kadebot - Telegram chat bot, using message intent to improve chat')
     parser.add_argument(
         '-c',
         '--config',
         type=str,
-        help=
-        f'Path to your config.json, if not placed in the default location ({os.path.join(conf.config, "config.json")})')
-    args = parser.parse_args()
+        help='Path to your config.json, if not placed in the default location ($XDG_CONFIG_HOME/kadebot/config.json)')
+    parser.parse_args()
     setup_bot(args)
 
 
